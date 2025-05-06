f2e1f2693adac02d481c2ec3d56875d3fa0ee233
miriyan19425 <miriyan19425@gmail.com>
2025-05-06 23:07:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
+import {philosophers} from './data.js';
 import Header from './components/Header';
 import PhilosophersList from './components/PhilosophersList';
+import PhilosopherDetails from './components/PhilosopherDetails';
 
 function App() {
+
+  const [selected, setSelected] = useState(null);
+   
+	function select(x) {
+		setSelected(x);  
+	}
+  
   return (
     <>
       <Header />
-      <main>
-        <PhilosophersList />
-      </main>  
+      <PhilosophersList philosophers={philosophers} handleSelect={select}/>
+      {selected && <PhilosopherDetails selectedPhilosopher={selected}/>}    
     </>
   );
 }
