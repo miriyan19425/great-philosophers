979b417c692c541fad9bcc59723f097395800ecf
miriyan19425 <miriyan19425@gmail.com>
2025-06-18 23:06:50 +0300

    Update Philosopher.jsx

--- src/components/Philosopher.jsx
+++ src/components/Philosopher.jsx
@@ -1,9 +1,11 @@
+import styles from './Header.module.css';
+
 function Philosopher({philosopher, handleOnClick}) {
   return (
 		<>
-			<img src={philosopher.image} alt={philosopher.name} width={philosopher.width} height={philosopher.height} />
-			<p>{philosopher.name}</p>
-			<button type='button' onClick={handleOnClick}>More</button>
+			<img src={philosopher.image} alt={philosopher.name} width={philosopher.width} height={philosopher.height} className={styles.pic}/>
+			<p className={styles.name}>{philosopher.name}</p>
+			<button type='button' onClick={handleOnClick} className={styles.moreButton}>More</button>
 		</>
 	);
 }
