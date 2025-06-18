83d0c6aead2c7c2bd12de495d9d6d9d397ea4175
miriyan19425 <miriyan19425@gmail.com>
2025-06-18 23:02:53 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
+import styles from './Header.module.css';
+
 function Header() {
-	return <header><h1>GREAT PHILOSOPHERS</h1></header>;	
+	return <header className={styles.header}><h1 className={styles.title}>GREAT PHILOSOPHERS</h1></header>;	
 }
 
 export default Header;
