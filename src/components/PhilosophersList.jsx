d4f2046364337a6f78052bebde61e2c86fe05997
miriyan19425 <miriyan19425@gmail.com>
2025-06-19 23:50:33 +0300

    Update PhilosophersList.jsx

--- src/components/PhilosophersList.jsx
+++ src/components/PhilosophersList.jsx
@@ -1,7 +1,8 @@
 import Philosopher from './Philosopher';
+import styles from './PhilosophersList.module.css';
 
 function PhilosophersList({philosophers, handleSelect}){
-	return <ul>{ philosophers.map( phil => <li key={phil.name}><Philosopher philosopher={phil} handleOnClick={() => handleSelect(phil)} /></li> ) }</ul>;
+	return <ul className={styles.list}>{ philosophers.map( phil => <li className={styles.item} key={phil.name}><Philosopher philosopher={phil} handleOnClick={() => handleSelect(phil)} /></li> ) }</ul>;
 }
 
 export default PhilosophersList;
